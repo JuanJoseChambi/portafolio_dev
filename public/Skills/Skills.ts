6d5f89bed00c5f7292a331369c2dfbe0e7269363
JoseChambi <chambijuanjose05@gmail.com>
2024-11-30 00:14:21 -0300

    [CH] Card Project with img of technologies

--- public/Skills/Skills.ts
+++ public/Skills/Skills.ts
@@ -2,10 +2,10 @@
 const svgmodules : Record<string, { default: string }> = import.meta.glob("./*.svg",{ eager: true });
 // console.log(svgmodules);
 
-export const svgs = Object.entries(svgmodules).map(([path, module]: [string ,{default: string}]) => ({
+const svgs = Object.entries(svgmodules).map(([path, module]: [string ,{default: string}]) => ({
     path:`Skills/${path.split("/")[1]}`,
     name: path.split("/")[1].split(".")[0],
     content: module.default
 }));
 
-
+export default svgs.map((svg) => ({...svg, name:svg.name.slice(2)}));
